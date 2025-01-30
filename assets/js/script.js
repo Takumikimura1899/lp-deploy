682a8b8f43b32d2d1dc0866239d0f454ef658f04
Takumikimura1899 <kimura141899@gmail.com>
2025-01-31 07:27:21 +0900

    ディレクトリ移動

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const accordionHeaders = document.querySelectorAll('.faq-accordion-header');
+  const activeIndex = 0;
+
+  accordionHeaders.forEach((header, index) => {
+    header.addEventListener('click', () => {
+      const content = header.nextElementSibling;
+
+      // クリックされたヘッダーに active クラスをトグル
+      header.classList.toggle('active');
+
+      // 他のアコーディオンを閉じる処理 (オプション)
+      accordionHeaders.forEach((otherHeader) => {
+        if (otherHeader !== header) {
+          otherHeader.classList.remove('active');
+          otherHeader.nextElementSibling.style.maxHeight = null;
+        }
+      });
+
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + 'px';
+      }
+    });
+
+    if (index === activeIndex) {
+      header.classList.add('active');
+      header.nextElementSibling.style.maxHeight = header.nextElementSibling.scrollHeight + 'px';
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const studioButtons = document.querySelectorAll('.studio-button');
+  const studioInfoContainers = document.querySelectorAll('.studio-info-container');
+  const closeButtons = document.querySelectorAll('.close-button');
+
+  const initiallyActiveStudio = 'jiyugaoka';
+
+  studioButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      studioButtons.forEach((btn) => btn.classList.remove('active'));
+      button.classList.add('active');
+      studioInfoContainers.forEach((container) => (container.style.display = 'none'));
+
+      const targetContainerId = `studio-info-container-${button.dataset.studio}`;
+      const targetContainer = document.getElementById(targetContainerId);
+      if (targetContainer) {
+        targetContainer.style.display = 'block';
+      }
+    });
+
+    if (button.dataset.studio === initiallyActiveStudio) {
+      button.classList.add('active');
+    }
+  });
+
+  closeButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const targetContainerId = button.dataset.target;
+      const targetContainer = document.getElementById(targetContainerId);
+
+      if (targetContainer) {
+        targetContainer.style.display = 'none';
+      }
+
+      studioButtons.forEach((btn) => btn.classList.remove('active'));
+    });
+  });
+
+  const initiallyActiveContainer = document.getElementById(
+    `studio-info-container-${initiallyActiveStudio}`,
+  );
+  if (initiallyActiveContainer) {
+    initiallyActiveContainer.style.display = 'block';
+  }
+});
